7181bb3387d4def5ec1a23d4a30f8ab31bdfa453
Tom Bosch <t.bosch@codezilla.consulting>
2024-08-21 17:33:14 +0200

    added WS-Config

---
+++ src/main/java/com/tomB/chatApp/config/WebsocketConfig.java
@@ -0,0 +1,30 @@
+package com.tomB.chatApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+//Message Broker verteilt Nachrichten
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        //an alle Clients
+        registry.enableSimpleBroker("/chatroom");
+        //für einzelnen Client
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //Clients können sich mit der Route verbinden
+        //withSockJS: JS-Library -> falls Websocket nicht verfügbar wird bspw. polling genutzt
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+
+    }
+}
